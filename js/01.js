e60d0f7ef21232e97161d5c3d33ecd728506449f
Josue_Lopez <josuedlopez1982@gmail.com>
2021-12-05 17:08:10 -0300

    Fundamentos de JS variables tipo let

---
+++ js/01.js
@@ -0,0 +1,22 @@
+/* Variables */
+
+/* Variables let las puedo iniciar vacías y les puedo reasignar el valor*/
+
+let cliente 
+console.log(cliente)
+cliente = 'Josue Lopez'
+console.log(cliente)
+let descuento
+console.log(descuento)
+descuento = 20
+console.log(descuento)
+
+console.log(`El cliente ${cliente} goza de un descuento del ${descuento}%`)
+
+let descuentoAniversario
+console.log(descuentoAniversario)
+descuentoAniversario = 10
+console.log(descuentoAniversario)
+descuentoAniversario = 15
+console.log(descuentoAniversario)
+
