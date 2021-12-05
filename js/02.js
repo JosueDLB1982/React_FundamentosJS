6ec13634ab244aece9a841e913e71648126c5369
Josue_Lopez <josuedlopez1982@gmail.com>
2021-12-05 17:46:24 -0300

    Introduccion a JS / Variables de tipo const

---
+++ js/02.js
@@ -0,0 +1,22 @@
+/* 
+    Variables tipo const
+
+    su valor no puede ser reasignado
+    no se pueden inicializar vacias
+*/
+
+const cliente = 'Josue Lopez'
+const precioDescuento = 200
+
+console.log(cliente)
+console.log(precioDescuento)
+
+const descuentoTemporada
+console.log(descuentoTemporada) // me marca error en la consola porque la variable no ha sido iniciada
+
+/* 
+    Reglas para nombrar variabnles
+
+    Las variables no pueden iniciar con números, punto o carárcteres especiales, a excepción del guión bajo
+    Los nombres de variables no pueden tener espacios en blanco
+*/
