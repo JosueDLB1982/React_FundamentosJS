c1ba598b8d30c18b51e9c5aadb73af3255a4ab9b
Josue_Lopez <josuedlopez1982@gmail.com>
2021-12-07 16:35:29 -0300

    Fundamentos de JS / Destructuring de arrays

---
+++ js/10.js
@@ -0,0 +1,12 @@
+// Destructuring de Arrays
+
+const technologies = [
+    'HTML',
+    'Javascript',
+    'Python',
+    'Java',
+    'CSS'
+]
+
+const [html,,,java] = technologies // Dejo los
+console.log(html, java)
