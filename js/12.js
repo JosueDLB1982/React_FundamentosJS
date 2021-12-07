abb4a8ccff1b9ebd923a01d7ca028d750eb2c90a
Josue_Lopez <josuedlopez1982@gmail.com>
2021-12-07 20:33:18 -0300

    Fundamentos de JS / Funciones

---
+++ js/12.js
@@ -0,0 +1,31 @@
+// Funciones
+
+// const addition = ((numberA, numberB) => {
+//     const result = (numberA +numberB)
+//     return result
+// })
+// console.log(addition(1,2))
+
+// const addition = ((numberA, numberB) => {
+//     return numberA + numberB
+// })
+// const result = addition(1,2)
+// console.log(result)
+
+// const addition = ((numberA, numberB) => {
+//     return [numberA + numberB, 'Hola Josue. estamos haciendo destructuring en el return']
+// })
+// const [result, mensaje] = addition(1,2) // array destructuring. Esto será muy común al ver hooks en React. useState
+// console.log(`La suma da: ${result}`)
+// console.log(mensaje)
+
+// retornar objeto
+const addition = ((numberA, numberB) => {
+    return {
+        result: numberA + numberB,
+        message: 'Hola Josue. estamos haciendo destructuring en el return'
+    }
+})
+const {result, message} = addition(1,2) // destructuring de objeto
+console.log(result) // imprimo result
+console.log(message) // imprimo el mensaje
